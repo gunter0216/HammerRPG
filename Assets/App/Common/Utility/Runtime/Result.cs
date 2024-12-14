f54fb7e0d143894695165a06d6bd322465879406
Nikita Ermolaev <nikita.er@danko.team>
2024-12-14 17:38:07 +0700

    добавил результ

---
+++ Assets/App/Common/Utility/Runtime/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Utils
+{
+    public struct Result<T>
+    {
+        public readonly T Object;
+        public readonly bool IsExist;
+
+        public Result(T result, bool isExist)
+        {
+            Object = result;
+            IsExist = isExist;
+        }
+        
+        public static Result<T> Success(T result)
+        {
+            return new Result<T>(result, true);
+        }
+
+        public static Result<T> Fail()
+        {
+            return new Result<T>(default, false);
+        }
+    }
+}
