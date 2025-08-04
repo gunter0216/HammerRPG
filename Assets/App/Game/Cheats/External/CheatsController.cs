f427b669f2aa5c097323e6415bd9816107839361
Nikita Ermolaev <Gunter0216@yandex.ru>
2025-08-04 16:15:28 +0700

    armor

--- Assets/App/Game/Cheats/External/CheatsController.cs
+++ Assets/App/Game/Cheats/External/CheatsController.cs
@@ -1,7 +1,9 @@
 ﻿using App.Common.Autumn.Runtime.Attributes;
 using App.Common.FSM.Runtime;
 using App.Common.FSM.Runtime.Attributes;
+using App.Common.ModuleItem.Runtime;
 using App.Game.Contexts;
+using App.Game.GameItems.External;
 using App.Game.States.Game;
 
 namespace App.Game.Cheats.External
@@ -10,8 +12,11 @@ namespace App.Game.Cheats.External
     [Stage(typeof(GameInitPhase), 0)]
     public class CheatsController : IInitSystem
     {
+        [Inject] private readonly IModuleItemsManager m_ModuleItemsManager;
+        
         public void Init()
         {
+            var configs = m_ModuleItemsManager.GetConfigs(GameItemsConstants.ModuleItemType);
             
         }
     }
